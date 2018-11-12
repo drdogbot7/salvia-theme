44249ff46d58525a3b6ae7d41f0741898b8327d7
Jeremy Mullis <jeremy.mullis@gmail.com>
2018-11-12 14:21:27 -0600

    remove sidebar, remove bootstrap cruft, update tailwind.js

--- lib/setup.php
+++ lib/setup.php
@@ -75,23 +75,6 @@ function widgets_init() {
 add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
 
 /**
- * Determine which pages should NOT display the sidebar
- */
-function display_sidebar() {
-  static $display;
-
-  isset($display) || $display = !in_array(true, [
-    // The sidebar will NOT be displayed if ANY of the following return true.
-    // @link https://codex.wordpress.org/Conditional_Tags
-    is_404(),
-    is_front_page(),
-    is_page_template('template-custom.php'),
-  ]);
-
-  return apply_filters('sage/display_sidebar', $display);
-}
-
-/**
  * Theme assets
  */
 function assets() {

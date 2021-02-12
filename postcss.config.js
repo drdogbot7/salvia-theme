90ac4c061fbd540a1da825ef3fcef55d11b9ba71
Jeremy Mullis <jeremy.mullis@gmail.com>
2021-02-12 15:31:04 -0600

    lots of cleanup

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,9 @@
-const postcssImport = require('postcss-easy-import');
 const tailwindcss = require('tailwindcss');
 const postcssPresetEnv = require('postcss-preset-env');
-const postcssExtend = require('postcss-extend');
 
 module.exports = {
 	plugins: [
-		postcssImport(),
 		tailwindcss('src/tailwind.config.js'),
-		postcssExtend(),
 		postcssPresetEnv({ stage: 0 }),
 	],
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import jQuery from 'jquery';
-import toggleMenu from './components/toggleMenu';
 import { addBackToTop } from 'vanilla-back-to-top';
 
 import './styles/main.scss';
 
 const initPage = () => {
-	document.getElementById('hamburger').addEventListener('click', function () {
-		toggleMenu(this);
-	});
 	addBackToTop({ backgroundColor: '#008FD5' });
 };
 
 /** Load Events */
-jQuery(document).ready(() => initPage());
+jQuery(function () {
+	initPage();
+});

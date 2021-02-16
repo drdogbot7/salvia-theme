0fe6ad1d003ab5047603e179c7660d10029424eb
Jeremy Mullis <jeremy.mullis@gmail.com>
2021-02-16 14:24:18 -0600

    support password-protected pages

--- page.php
+++ page.php
@@ -1,4 +1,10 @@
 <?php
-$context = Timber::get_context();
+$context         = Timber::context();
+$timber_post     = Timber::get_post();
+$context['post'] = $timber_post;
 
-Timber::render('page.twig', $context);
+if ( post_password_required( $timber_post->ID ) ) {
+	Timber::render( 'single-password.twig', $context );
+} else {
+	Timber::render( array( 'page-' . $timber_post->ID . '.twig', 'page-' . $timber_post->post_type . '.twig', 'page-' . $timber_post->slug . '.twig', 'page.twig' ), $context );
+}

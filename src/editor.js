40154f026789a6e422896cb86bfb48a9749595ef
Jeremy Mullis <jeremy.mullis@gmail.com>
2022-02-17 16:26:57 -0600

    remove wp-block-separator options

--- src/editor.js
+++ src/editor.js
@@ -1,1 +1,9 @@
 import './styles/editor.scss';
+
+/**
+ *  Remove some block style
+ */
+wp.domReady(() => {
+	wp.blocks.unregisterBlockStyle('core/separator', 'dots');
+	wp.blocks.unregisterBlockStyle('core/separator', 'wide');
+});

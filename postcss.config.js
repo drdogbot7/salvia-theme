f2857e2e751fe7167e19afb3c620876d5fdb6fd9
Jeremy Mullis <jeremy.mullis@gmail.com>
2022-03-03 09:49:30 -0600

    add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,15 @@
-const tailwindcss = require('tailwindcss');
-const postcssPresetEnv = require('postcss-preset-env');
-
-module.exports = {
-	plugins: [
-		tailwindcss('src/tailwind.config.js'),
-		postcssPresetEnv({ stage: 0 }),
-	],
+const postcssConfig = {
+	plugins: [require('tailwindcss'), require('postcss-preset-env')],
 };
+
+if (process.env.NODE_ENV === 'production') {
+	postcssConfig.plugins.push(
+		require('cssnano')({
+			// use the safe preset so that it doesn't
+			// mutate or remove code from our css
+			preset: 'default',
+		})
+	);
+}
+
+module.exports = postcssConfig;

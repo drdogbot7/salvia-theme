75649471f496be7bb15d3b27994c710ae1d63ffe
Jeremy Mullis <jeremy.mullis@gmail.com>
2022-04-08 14:55:15 -0500

    add button block variations

--- src/editor.js
+++ src/editor.js
@@ -1,9 +1,17 @@
 import './styles/editor.scss';
 
 /**
- *  Remove some block style
+ *  Add and Remove block styles
  */
 wp.domReady(() => {
 	wp.blocks.unregisterBlockStyle('core/separator', 'dots');
 	wp.blocks.unregisterBlockStyle('core/separator', 'wide');
+	wp.blocks.registerBlockStyle('core/button', {
+		name: 'small',
+		label: 'Small',
+	});
+	wp.blocks.registerBlockStyle('core/button', {
+		name: 'large',
+		label: 'Large',
+	});
 });

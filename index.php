d7ef0bbdfc1edb626d7433f479c926923b5e09f1
Jeremy <jeremy.mullis@gmail.com>
2022-05-25 13:01:36 -0600

    use front_page.twig, home.twig if available

--- index.php
+++ index.php
@@ -9,4 +9,14 @@
 
 $context = Timber::context();
 
-Timber::render('index.twig', $context);
+$templates = ['index.twig'];
+
+if (is_home()) {
+	array_unshift($templates, 'home.twig');
+}
+
+if (is_front_page()) {
+	array_unshift($templates, 'front_page.twig');
+}
+
+Timber::render($templates, $context);

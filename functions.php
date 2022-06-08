a3b1f3a5309b6eed526468432ba1ce02a06ad387
Jeremy Mullis <jeremy.mullis@gmail.com>
2022-06-08 16:04:34 -0500

    disable comments

--- functions.php
+++ functions.php
@@ -23,6 +23,10 @@ add_action('pre_get_posts', 'salvia_pre_get_posts');
 add_action('wp_enqueue_scripts', 'salvia_enqueue_front', 100);
 add_action('enqueue_block_editor_assets', 'salvia_enqueue_editor', 100);
 
+// Disable Comments
+add_action('init', 'salvia_disable_comments');
+add_action('admin_init', 'salvia_disable_comments_admin');
+
 function salvia_add_to_context($context)
 {
 	$context['menu'] = Timber::get_menu('primary_navigation');
@@ -131,4 +135,44 @@ function salvia_pre_get_posts($query)
 	// 	$query->set( 'posts_per_page', -1 );
 	// 	return;
 	// }
+}
+
+function salvia_disable_comments()
+{
+	// Close comments on the front-end
+	add_filter('comments_open', '__return_false', 20, 2);
+	add_filter('pings_open', '__return_false', 20, 2);
+
+	// Hide existing comments
+	add_filter('comments_array', '__return_empty_array', 10, 2);
+
+	// Remove comments links from admin bar
+	if (is_admin_bar_showing()) {
+		remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+	}
+}
+
+function salvia_disable_comments_admin()
+{
+	// Redirect any user trying to access comments page
+	global $pagenow;
+
+	if ($pagenow === 'edit-comments.php') {
+		wp_redirect(admin_url());
+		exit();
+	}
+
+	// Remove comments metabox from dashboard
+	remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+
+	// Disable support for comments and trackbacks in post types
+	foreach (get_post_types() as $post_type) {
+		if (post_type_supports($post_type, 'comments')) {
+			remove_post_type_support($post_type, 'comments');
+			remove_post_type_support($post_type, 'trackbacks');
+		}
+	}
+
+	// Remove comments page in menu
+	remove_menu_page('edit-comments.php');
 }
